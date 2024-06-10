f714e66aa7ab6f9bd2aa11155b9696f6fcd03d63
MicheleLafronza <michele.lafronza91@gmail.com>
2024-06-10 16:35:43 +0200

    feat: add event click on next image

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 // creo costante del container
 const container = document.querySelector(".container");
-// console.log(container);
+
 
 // creo array con le stringhe delle immagini
 const imgPath = [
     "img/01.webp", "img/02.webp", "img/03.webp", "img/04.webp", "img/05.webp",
 ];
-// console.log(imgPath);
 
-// setto il ciclo che andrà ad inserire automaticamente nel html dentro il container, il div e il tag img
+
+// setto il ciclo che andrà ad inserire automaticamente l'html dentro il container, il div e il tag img
 for (i = 0; i < imgPath.length; i++) {
     let imgIt = imgPath[i];
-    // console.log(imgIt);
+
 
     // creo la parte html da mettere dento il container
     let divIt = `
@@ -24,6 +24,44 @@ for (i = 0; i < imgPath.length; i++) {
                     <i class="fa-solid fa-angle-down"></i>
             </a>
         </div>`;
-    // appendo la parte nei backtick nel container
-    container.innerHTML += divIt
+        
+    // aggiungo la parte nei backtick al container
+    container.innerHTML += divIt;
 }
+
+// creo varibile dei tasti prev e next
+let next = document.querySelector(".next");
+
+
+let prev = document.querySelector(".prev");
+
+
+// creo la lista della classe items
+const items = document.getElementsByClassName("item");
+console.log(items);
+
+// creo variabile "t" che conta come item attivo
+let t = 0;
+
+
+// aggiungo la classe active al primo elemento della lista items
+items[t].classList.add("active");
+
+
+// evento click next
+next.addEventListener("click",
+    function() {
+
+        // rimuovo la classe active dall'elemento precedente
+        items[t].classList.remove("active");
+
+        // aumento il valore di "t"
+        t++;
+
+        // assegno al nuovo "t" la classe active
+        items[t].classList.add("active");
+        
+        console.log(t);
+    }
+
+)
